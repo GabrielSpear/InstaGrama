a28417841983fc77c4287d416bb5d8d8e7b93d3e
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-16 17:22:36 +0400

    Timeline function added

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Login  function, display user profile function, displaying user_profile image, find_profile
+
+@login_required(login_url='/accounts/login/')
+def timelines(request):
+    current_user = request.user
+    images = Image.objects.order_by('-date_uploaded')
+    profiles = Profile.objects.order_by('-last_update')
+    comments = Comments.objects.order_by('-time_comment')
+    return render(request, 'timelines.html', {'images': images, 'profiles': profiles, 'user_profile': user_profile, 'comments': comments})
