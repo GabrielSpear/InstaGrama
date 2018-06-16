765f8cd1f5c17f817641d89dbbba2ba3e138bb50
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-16 17:23:37 +0400

    Profile view function added

--- gram/views.py
+++ gram/views.py
@@ -10,3 +10,10 @@ def timelines(request):
     profiles = Profile.objects.order_by('-last_update')
     comments = Comments.objects.order_by('-time_comment')
     return render(request, 'timelines.html', {'images': images, 'profiles': profiles, 'user_profile': user_profile, 'comments': comments})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.get(user_id=current_user)
+    images = Image.objects.all().filter(user=current_user)
+    return render(request, 'profile.html', {'images': images, 'profile': profile})
