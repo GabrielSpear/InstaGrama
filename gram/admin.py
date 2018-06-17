cf8fa0cbc50cc4dfde329501ee9ea29e409ad9d5
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-17 11:59:24 +0400

    registering Image & profile models

--- gram/admin.py
+++ gram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image, Profile
+from django.contrib.sites.models import Site
 
 # Register your models here.
+# Register image and profile model
+admin.site.register(Image)
+admin.site.register(Profile)
