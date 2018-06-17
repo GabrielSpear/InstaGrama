423e008285a4477118f73475d5057fd2dda4242b
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-17 12:07:06 +0400

    image like and comment urls added

--- gram/urls.py
+++ gram/urls.py
@@ -11,4 +11,12 @@ urlpatterns = [
     url(r'^user/(\d+)', views.user_profile, name='userProfiles'),
     url(r'^image/(\d+)', views.single_image, name='singleImage'),
     url(r'^profile/', views.find_profile, name='findProfile'),
-    
+    url(r'^single_image/likes/(\d+)',
+        views.single_image_like, name='singleImageLike'),
+    url(r'^new/comment/(?P<username>[-_\w.]+)$',
+        views.new_comment, name='newComment'),
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
