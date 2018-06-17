f190acef867e3da7f1557f5b39f4dfe5a483f740
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-17 12:04:39 +0400

    new status & user profile url added and working

--- gram/urls.py
+++ gram/urls.py
@@ -6,3 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.timelines, name='allTimelines'),
     url(r'^accounts/profile/$', views.profile, name='myProfile'),
+    url(r'^new/status/(?P<username>[-_\w.]+)$',
+        views.new_status, name='newStatus'),
+    url(r'^user/(\d+)', views.user_profile, name='userProfiles'),
