314159f513ff47862fcf4a0575e5da65a2429fc9
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-17 12:01:12 +0400

    Image class test

---
+++ gram/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+import datetime as dt
+from .models import Image, Profile
+from django.contrib.auth.models import User
+
+# Create your tests here.
+# Create image view function
+
+
+class ImageTestClass(TestCase):
+
+    def setUp(self):
+        self.image = Image(image='imageurl', image_name='nature',
+                           image_caption='wowo', likes=200, comments='wonderful')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
