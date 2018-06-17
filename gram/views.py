89ee6c1cdda7955f22e22719272183097830059b
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-17 12:10:57 +0400

    Added admin panel

--- gram/views.py
+++ gram/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import NewStatusForm, NewCommentForm
+from .models import Image, Profile, Comments
 
 # Create your views here.
 # Login  function, display user profile function, displaying user_profile image, find_profile
