55f71cf03100f620d0c1ce8c834e9590d72b565e
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-18 17:00:37 +0400

    Deployment

--- Insta/settings.py
+++ Insta/settings.py
@@ -33,7 +33,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG',default=False, cast=bool)
 ALLOWED_HOSTS = ['*']
-
 # Application definition
 
 INSTALLED_APPS = [
